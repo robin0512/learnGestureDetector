b5b8b478eec6a4edc0c23cf4111752f4e38cff6b
RobinHoo <53432684+robin0512@users.noreply.github.com>
2024-01-16 23:06:31 +0800

    add basic gesture

--- app/src/main/java/com/example/learngesturedetector/MainActivity.kt
+++ app/src/main/java/com/example/learngesturedetector/MainActivity.kt
@@ -1,11 +1,89 @@
 package com.example.learngesturedetector
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.example.learngesturedetector.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var gestureDetector: GestureDetector
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        gestureDetector = GestureDetector(this, GestureListener())
+        binding.gestureView.setOnTouchListener(touchListener)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private val touchListener = View.OnTouchListener { view, event ->
+        handleTouch(view, event)
+        gestureDetector.onTouchEvent(event)
+    }
+
+    private fun handleTouch(view: View, event: MotionEvent) {
+        val drawable =
+            ContextCompat.getDrawable(this, R.drawable.rounded_background) as GradientDrawable
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                drawable.setColor(ContextCompat.getColor(this, R.color.colorPressed))
+            }
+
+            MotionEvent.ACTION_UP -> {
+                drawable.setColor(ContextCompat.getColor(this, R.color.colorDefault))
+            }
+        }
+        view.background = drawable
+    }
+
+    private fun updateGestureStatusText(text: String) {
+        binding.gestureText.text = text
+    }
+
+    inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            updateGestureStatusText("onSingleTapUp")
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            updateGestureStatusText("onLongPress")
+        }
+
+        override fun onDoubleTap(e: MotionEvent): Boolean {
+            updateGestureStatusText("onDoubleTap")
+            return true
+        }
+
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            updateGestureStatusText("onFling")
+            return true
+        }
+
+        override fun onScroll(
+            e1: MotionEvent?,
+            e2: MotionEvent,
+            distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            updateGestureStatusText("onScroll")
+            return true
+        }
     }
 }
