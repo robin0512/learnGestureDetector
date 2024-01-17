68715d8638773cbb820ca333fa2bf38bb9a4ef6d
RobinHoo <53432684+robin0512@users.noreply.github.com>
2024-01-17 21:55:55 +0800

    update gesture

--- app/src/main/java/com/example/learngesturedetector/MainActivity.kt
+++ app/src/main/java/com/example/learngesturedetector/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
@@ -54,15 +55,41 @@ class MainActivity : AppCompatActivity() {
 
         override fun onSingleTapUp(e: MotionEvent): Boolean {
             updateGestureStatusText("onSingleTapUp")
+            Log.i("GestureStatus","onSingleTapUp")
             return true
         }
 
         override fun onLongPress(e: MotionEvent) {
             updateGestureStatusText("onLongPress")
+            Log.i("GestureStatus","onLongPress")
         }
 
         override fun onDoubleTap(e: MotionEvent): Boolean {
             updateGestureStatusText("onDoubleTap")
+            Log.i("GestureStatus","onDoubleTap")
+            return true
+        }
+
+        override fun onDoubleTapEvent(e: MotionEvent): Boolean {
+//            updateGestureStatusText("onDoubleTapEvent")
+            Log.i("GestureStatus","onDoubleTapEvent")
+            return true
+        }
+
+        override fun onDown(e: MotionEvent): Boolean {
+//            updateGestureStatusText("onDown")
+            Log.i("GestureStatus","onDown")
+            return true
+        }
+
+        override fun onShowPress(e: MotionEvent) {
+//            updateGestureStatusText("onShowPress")
+            Log.i("GestureStatus","onShowPress")
+        }
+
+        override fun onSingleTapConfirmed(e: MotionEvent): Boolean {
+//            updateGestureStatusText("onSingleTapConfirmed")
+            Log.i("GestureStatus","onSingleTapConfirmed")
             return true
         }
 
@@ -73,6 +100,7 @@ class MainActivity : AppCompatActivity() {
             velocityY: Float
         ): Boolean {
             updateGestureStatusText("onFling")
+            Log.i("GestureStatus","onFling")
             return true
         }
 
@@ -83,6 +111,7 @@ class MainActivity : AppCompatActivity() {
             distanceY: Float
         ): Boolean {
             updateGestureStatusText("onScroll")
+            Log.i("GestureStatus","onScroll")
             return true
         }
     }
